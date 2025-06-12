68f56f3fd0039a0f62b2c021b3cb9b6b96d718ad
AKEKOFranck <fa0055970@gmail.com>
2025-06-12 19:49:48 +0000

    new resolution

--- src/index.js
+++ src/index.js
@@ -18,3 +18,16 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// Enregistrement du service worker pour PWA
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(registration => {
+        console.log('Service Worker enregistré avec succès:', registration);
+      })
+      .catch(error => {
+        console.log('Erreur lors de l\'enregistrement du Service Worker:', error);
+      });
+  });
+}
